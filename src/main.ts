b641c3fda828abbbae6c30d546126b196bff3cfe
pvdevs <paulovictor.devs@gmail.com>
2024-08-13 05:28:32 -0300

    WIP

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.getOrThrow<number>('PORT');
+  console.log(`PORT: ${port}`);
+
   await app.listen(port, () => console.log(`Running on port ${port}`));
 }
 bootstrap();
